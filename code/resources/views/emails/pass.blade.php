ceec1cb9b47a06da9f45b4c602f5f62626d08d7e
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2015-11-30 19:06:32 +0530

    Update pass.blade.php

--- code/resources/views/emails/pass.blade.php
+++ code/resources/views/emails/pass.blade.php
@@ -2,7 +2,7 @@ Hello {!! $name !!}, <br/><br/>
 
 This email is confirmation that you are now registered at our helpdesk. <br/> <br/> 
 
-Registered email: <br/>
+Registered email: {!! $emailadd !!} <br/>
 Password: {{$password}} <br/> <br/> 
 
 You can visit the helpdesk to browse articles and contact us at any time: {!! \URL::route('ticket2') !!}<br/> <br/> 
