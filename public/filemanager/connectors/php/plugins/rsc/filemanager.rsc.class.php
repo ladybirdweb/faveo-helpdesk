14bf19ec4ee608ee06633b4c4919fd41528b3f7a
Manish Verma <mverma16@outlook.com>
2016-02-19 11:18:43 +0530

    Update filemanager.rsc.class.php

--- public/filemanager/connectors/php/plugins/rsc/filemanager.rsc.class.php
+++ public/filemanager/connectors/php/plugins/rsc/filemanager.rsc.class.php
@@ -105,7 +105,7 @@ class FilemanagerRSC extends Filemanager {
 			foreach ( $objects as $object ) {
 				if(!isset($this->params['type']) || (isset($this->params['type']) && strtolower($this->params['type'])=='images' && in_array(strtolower($object->content_type),$this->config['images']))) {
 					if($this->config['upload']['imagesonly']== false || ($this->config['upload']['imagesonly']== true && in_array(strtolower($object->content_type),$this->config['images']))) {
-						$object = $this->get_file_info(&$object);
+						$object = $this->get_file_info($object);
 						$array[$object->url] = array(
 							'Path'=> $object->path,
 							'Filename' => $object->name,
