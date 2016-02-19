146839df13f743166d7439584b4cf9b0ee7eeb57
Manish Verma <mverma16@outlook.com>
2016-02-19 05:24:10 -0500

    Applied fixes from StyleCI

--- app/Http/Controllers/Admin/helpdesk/EmailsController.php
+++ app/Http/Controllers/Admin/helpdesk/EmailsController.php
@@ -11,39 +11,43 @@ use App\Http\Requests\helpdesk\EmailsRequest;
 use App\Model\helpdesk\Agent\Department;
 use App\Model\helpdesk\Email\Emails;
 use App\Model\helpdesk\Manage\Help_topic;
-use App\Model\helpdesk\Utility\MailboxProtocol;
-use App\Model\helpdesk\Ticket\Ticket_Priority;
 use App\Model\helpdesk\Settings\Email;
+use App\Model\helpdesk\Ticket\Ticket_Priority;
+use App\Model\helpdesk\Utility\MailboxProtocol;
 // classes
 use Crypt;
 use Exception;
 
 /**
- * EmailsController
+ * EmailsController.
  *
- * @package     Controllers
- * @subpackage  Controller
  * @author      Ladybird <info@ladybirdweb.com>
  */
-class EmailsController extends Controller {
-
+class EmailsController extends Controller
+{
     /**
      * Create a new controller instance.
+     *
      * @return type
      */
-    public function __construct() {
+    public function __construct()
+    {
         $this->middleware('auth');
         $this->middleware('roles');
     }
 
     /**
      * Display a listing of the resource.
+     *
      * @param type Emails $emails
+     *
      * @return type Response
      */
-    public function index(Emails $emails) {
+    public function index(Emails $emails)
+    {
         try {
             $emails = $emails->get();
+
             return view('themes.default1.admin.helpdesk.emails.emails.index', compact('emails'));
         } catch (Exception $e) {
             return view('404');
@@ -52,18 +56,22 @@ class EmailsController extends Controller {
 
     /**
      * Show the form for creating a new resource.
-     * @param type Department $department
-     * @param type Help_topic $help
-     * @param type Priority $priority
+     *
+     * @param type Department      $department
+     * @param type Help_topic      $help
+     * @param type Priority        $priority
      * @param type MailboxProtocol $mailbox_protocol
+     *
      * @return type Response
      */
-    public function create(Department $department, Help_topic $help, Ticket_Priority $priority, MailboxProtocol $mailbox_protocol) {
+    public function create(Department $department, Help_topic $help, Ticket_Priority $priority, MailboxProtocol $mailbox_protocol)
+    {
         try {
             $departments = $department->get();
             $helps = $help->get();
             $priority = $priority->get();
             $mailbox_protocols = $mailbox_protocol->get();
+
             return view('themes.default1.admin.helpdesk.emails.emails.create', compact('mailbox_protocols', 'priority', 'departments', 'helps'));
         } catch (Exception $e) {
             return view('404');
@@ -72,11 +80,14 @@ class EmailsController extends Controller {
 
     /**
      * Store a newly created resource in storage.
-     * @param type Emails $email
+     *
+     * @param type Emails        $email
      * @param type EmailsRequest $request
+     *
      * @return type Response
      */
-    public function store(Emails $email, EmailsRequest $request) {
+    public function store(Emails $email, EmailsRequest $request)
+    {
         try {
             $password = $request->input('password');
             $encrypted = Crypt::encrypt($password);
@@ -106,6 +117,7 @@ class EmailsController extends Controller {
                 $email_settings = Email::where('id', '=', '1')->first();
                 $email_settings->sys_email = $email->id;
                 $email_settings->save();
+
                 return redirect('emails')->with('success', 'Email Created sucessfully');
             } else {
                 return redirect('emails')->with('fails', 'Email can not Create');
@@ -118,30 +130,36 @@ class EmailsController extends Controller {
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
-    public function show($id) {
+    public function show($id)
+    {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
-     * @param type int $id
-     * @param type Department $department
-     * @param type Help_topic $help
-     * @param type Emails $email
-     * @param type Priority $priority
+     *
+     * @param type int             $id
+     * @param type Department      $department
+     * @param type Help_topic      $help
+     * @param type Emails          $email
+     * @param type Priority        $priority
      * @param type MailboxProtocol $mailbox_protocol
+     *
      * @return type Response
      */
-    public function edit($id, Department $department, Help_topic $help, Emails $email, Ticket_Priority $priority, MailboxProtocol $mailbox_protocol) {
+    public function edit($id, Department $department, Help_topic $help, Emails $email, Ticket_Priority $priority, MailboxProtocol $mailbox_protocol)
+    {
         try {
             $emails = $email->whereId($id)->first();
             $departments = $department->get();
             $helps = $help->get();
             $priority = $priority->get();
             $mailbox_protocols = $mailbox_protocol->get();
+
             return view('themes.default1.admin.helpdesk.emails.emails.edit', compact('mailbox_protocols', 'priority', 'departments', 'helps', 'emails'));
         } catch (Exception $e) {
             return view('404');
@@ -150,12 +168,15 @@ class EmailsController extends Controller {
 
     /**
      * Update the specified resource in storage.
-     * @param type $id
-     * @param type Emails $email
+     *
+     * @param type                   $id
+     * @param type Emails            $email
      * @param type EmailsEditRequest $request
+     *
      * @return type Response
      */
-    public function update($id, Emails $email, EmailsEditRequest $request) {
+    public function update($id, Emails $email, EmailsEditRequest $request)
+    {
         $password = $request->input('password');
         $encrypted = Crypt::encrypt($password);
         //echo $encrypted;
@@ -182,6 +203,7 @@ class EmailsController extends Controller {
             }
             $emails->password = $encrypted;
             $emails->save();
+
             return redirect('emails')->with('success', 'Email Updated sucessfully');
         } catch (Exception $e) {
             return redirect('emails')->with('fails', 'Email not updated');
@@ -190,11 +212,14 @@ class EmailsController extends Controller {
 
     /**
      * Remove the specified resource from storage.
-     * @param type int $id
+     *
+     * @param type int    $id
      * @param type Emails $email
+     *
      * @return type Response
      */
-    public function destroy($id, Emails $email) {
+    public function destroy($id, Emails $email)
+    {
         $default_system_email = Email::where('id', '=', '1')->first();
         if ($default_system_email->id) {
             if ($id == $default_system_email->id) {
@@ -212,5 +237,4 @@ class EmailsController extends Controller {
             return redirect('emails')->with('fails', 'Email can not  Delete ');
         }
     }
-
 }
