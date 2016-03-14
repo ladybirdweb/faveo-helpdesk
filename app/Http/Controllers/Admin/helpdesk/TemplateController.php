02ab132b090e83124a476b65009431a274ee18eb
Manish Verma <manish.verma@ladybirdweb.com>
2016-03-14 01:05:00 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/Admin/helpdesk/TemplateController.php
+++ app/Http/Controllers/Admin/helpdesk/TemplateController.php
@@ -24,14 +24,15 @@ use Mail;
  *
  * @author      Ladybird <info@ladybirdweb.com>
  */
-class TemplateController extends Controller {
-
+class TemplateController extends Controller
+{
     /**
      * Create a new controller instance.
      *
      * @return type void
      */
-    public function __construct(PhpMailController $PhpMailController) {
+    public function __construct(PhpMailController $PhpMailController)
+    {
         $this->PhpMailController = $PhpMailController;
         SettingsController::smtp();
         $this->middleware('auth');
@@ -45,7 +46,8 @@ class TemplateController extends Controller {
      *
      * @return type Response
      */
-    public function index(Template $template) {
+    public function index(Template $template)
+    {
         try {
             $templates = $template->get();
 
@@ -63,7 +65,8 @@ class TemplateController extends Controller {
      *
      * @return type Response
      */
-    public function create(Languages $language, Template $template) {
+    public function create(Languages $language, Template $template)
+    {
         try {
             $templates = $template->get();
             $languages = $language->get();
@@ -82,7 +85,8 @@ class TemplateController extends Controller {
      *
      * @return type Response
      */
-    public function store(Template $template, TemplateRequest $request) {
+    public function store(Template $template, TemplateRequest $request)
+    {
         try {
             /* Check whether function success or not */
             if ($template->fill($request->input())->save() == true) {
@@ -105,7 +109,8 @@ class TemplateController extends Controller {
      *
      * @return Response
      */
-    public function show($id) {
+    public function show($id)
+    {
         //
     }
 
@@ -118,7 +123,8 @@ class TemplateController extends Controller {
      *
      * @return type Response
      */
-    public function listdirectories() {
+    public function listdirectories()
+    {
         $path = '../resources/views/emails/';
         $directories = scandir($path);
         $directory = str_replace('/', '-', $path);
@@ -126,29 +132,32 @@ class TemplateController extends Controller {
         return view('themes.default1.admin.helpdesk.emails.template.listdirectories', compact('directories', 'directory'));
     }
 
-    public function listtemplates($template, $path) {
+    public function listtemplates($template, $path)
+    {
         $paths = str_replace('-', '/', $path);
-        $directory2 = $paths . $template;
+        $directory2 = $paths.$template;
 
         $templates = scandir($directory2);
-        $directory = str_replace('/', '-', $directory2 . '/');
+        $directory = str_replace('/', '-', $directory2.'/');
 
         return view('themes.default1.admin.helpdesk.emails.template.listtemplates', compact('templates', 'directory'));
     }
 
-    public function readtemplate($template, $path) {
+    public function readtemplate($template, $path)
+    {
         $directory = str_replace('-', '/', $path);
-        $handle = fopen($directory . $template, 'r');
-        $contents = fread($handle, filesize($directory . $template));
+        $handle = fopen($directory.$template, 'r');
+        $contents = fread($handle, filesize($directory.$template));
         fclose($handle);
 
         return view('themes.default1.admin.helpdesk.emails.template.readtemplates', compact('contents', 'template', 'path'));
     }
 
-    public function createtemplate() {
+    public function createtemplate()
+    {
         $directory = '../resources/views/emails/';
         $fname = Input::get('folder_name');
-        $filename = $directory . $fname;
+        $filename = $directory.$fname;
 
 // images folder creation using php
 //   $mydir = dirname( __FILE__ )."/html/images";
@@ -160,7 +169,7 @@ class TemplateController extends Controller {
         if (!file_exists($filename)) {
             mkdir($filename, 0777);
         }
-        $files = array_filter(scandir($directory . 'default'));
+        $files = array_filter(scandir($directory.'default'));
 
         foreach ($files as $file) {
             if ($file === '.' or $file === '..') {
@@ -168,27 +177,29 @@ class TemplateController extends Controller {
             }
             if (!is_dir($file)) {
                 //   $file_to_go = str_replace("code/resources/views/emails/",'code/resources/views/emails/'.$fname,$file);
-                $destination = $directory . $fname . '/';
+                $destination = $directory.$fname.'/';
 
-                copy($directory . 'default/' . $file, $destination . $file);
+                copy($directory.'default/'.$file, $destination.$file);
             }
         }
 
         return \Redirect::back()->with('success', 'Successfully copied');
     }
 
-    public function writetemplate($template, $path) {
+    public function writetemplate($template, $path)
+    {
         $directory = str_replace('-', '/', $path);
         $b = Input::get('templatedata');
 
-        file_put_contents($directory . $template, print_r($b, true));
+        file_put_contents($directory.$template, print_r($b, true));
 
         return \Redirect::back()->with('success', 'Successfully updated');
     }
 
-    public function deletetemplate($template, $path) {
+    public function deletetemplate($template, $path)
+    {
         $directory = str_replace('-', '/', $path);
-        $dir = $directory . $template;
+        $dir = $directory.$template;
         $status = \DB::table('settings_email')->first();
         if ($template == 'default' or $template == $status->template) {
             return \Redirect::back()->with('fails', 'You cannot delete a default or active directory!');
@@ -197,7 +208,7 @@ class TemplateController extends Controller {
             $objects = scandir($dir);
             foreach ($objects as $object) {
                 if ($object != '.' && $object != '..') {
-                    unlink($dir . '/' . $object);
+                    unlink($dir.'/'.$object);
                 }
             }
             rmdir($dir);
@@ -208,13 +219,15 @@ class TemplateController extends Controller {
         return \Redirect::back()->with('success', 'Successfully Deleted');
     }
 
-    public function activateset($setname) {
+    public function activateset($setname)
+    {
         \DB::table('settings_email')->update(['template' => $setname]);
 
         return \Redirect::back()->with('success', 'You have Successfully Activated this Set');
     }
 
-    public function edit($id, Template $template, Languages $language) {
+    public function edit($id, Template $template, Languages $language)
+    {
         try {
             $templates = $template->whereId($id)->first();
             $languages = $language->get();
@@ -234,7 +247,8 @@ class TemplateController extends Controller {
      *
      * @return type Response
      */
-    public function update($id, Template $template, TemplateUdate $request) {
+    public function update($id, Template $template, TemplateUdate $request)
+    {
         try {
             //TODO validation
             $templates = $template->whereId($id)->first();
@@ -260,7 +274,8 @@ class TemplateController extends Controller {
      *
      * @return type Response
      */
-    public function destroy($id, Template $template) {
+    public function destroy($id, Template $template)
+    {
         try {
             $templates = $template->whereId($id)->first();
             /* Check whether function success or not */
@@ -284,7 +299,8 @@ class TemplateController extends Controller {
      *
      * @return type Response
      */
-    public function formDiagno(Emails $email) {
+    public function formDiagno(Emails $email)
+    {
         try {
             $emails = $email->get();
 
@@ -301,7 +317,8 @@ class TemplateController extends Controller {
      *
      * @return type
      */
-    public function postDiagno(Request $request) {
+    public function postDiagno(Request $request)
+    {
         $email = $request->input('to');
         if ($email == null) {
             return redirect('getdiagno')->with('fails', 'Please provide E-mail address !');
@@ -309,13 +326,10 @@ class TemplateController extends Controller {
         // sending mail via php mailer
         $mail = $this->PhpMailController->sendmail($from = $this->PhpMailController->mailfrom('1', '0'), $to = ['email' => $email], $message = ['subject' => 'Checking the connection', 'scenario' => 'error-report', 'content' => 'Email Received Successfully'], $template_variables = ['system_error' => 'hello']);
 
-        if($mail == null){
-            return redirect('getdiagno')->with('fails', 'Please check your E-mail settings. Unable to send mails');    
+        if ($mail == null) {
+            return redirect('getdiagno')->with('fails', 'Please check your E-mail settings. Unable to send mails');
         } else {
-            return redirect('getdiagno')->with('success', 'Please check your mail. An E-mail has been sent to your E-mail address');    
+            return redirect('getdiagno')->with('success', 'Please check your mail. An E-mail has been sent to your E-mail address');
         }
-
-        
     }
-
 }
