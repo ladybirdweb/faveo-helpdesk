cf92ee10e64225552de7222cb2787252884dbc55
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-04-19 09:52:10 -0400

    Applied fixes from StyleCI

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -68,6 +68,7 @@ class Handler extends ExceptionHandler
                         }
                     }
                 }
+
                 return response()->view('errors.500', []);
             }
         }
