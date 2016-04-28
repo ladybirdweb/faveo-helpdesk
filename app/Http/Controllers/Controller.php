0a309c448ec787ceeacb489372c0cde19f936c26
Laravel Shift <shift@laravelshift.com>
2016-04-28 01:54:53 -0400

    Various small shifts for Laravel 5.2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-abstract class Controller extends BaseController
+class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs,
         ValidatesRequests;
