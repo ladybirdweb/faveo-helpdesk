418fbbce7527b14a85510254a4c6eb176c2d5078
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-05-09 14:52:20 +0530

    Update v1.0.7.4

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -517,25 +517,25 @@ Route::get('notification', ['as' => 'notification', 'uses' => 'Agent\helpdesk\No
   |  View all the Routes
   |=============================================================
  */
-Route::get('/aaa', function () {
-    $routeCollection = Route::getRoutes();
-    echo "<table style='width:100%'>";
-    echo '<tr>';
-    echo "<td width='10%'><h4>HTTP Method</h4></td>";
-    echo "<td width='10%'><h4>Route</h4></td>";
-    echo "<td width='10%'><h4>Url</h4></td>";
-    echo "<td width='80%'><h4>Corresponding Action</h4></td>";
-    echo '</tr>';
-    foreach ($routeCollection as $value) {
-        echo '<tr>';
-        echo '<td>'.$value->getMethods()[0].'</td>';
-        echo '<td>'.$value->getName().'</td>';
-        echo '<td>'.$value->getPath().'</td>';
-        echo '<td>'.$value->getActionName().'</td>';
-        echo '</tr>';
-    }
-    echo '</table>';
-});
+// Route::get('/aaa', function () {
+//     $routeCollection = Route::getRoutes();
+//     echo "<table style='width:100%'>";
+//     echo '<tr>';
+//     echo "<td width='10%'><h4>HTTP Method</h4></td>";
+//     echo "<td width='10%'><h4>Route</h4></td>";
+//     echo "<td width='10%'><h4>Url</h4></td>";
+//     echo "<td width='80%'><h4>Corresponding Action</h4></td>";
+//     echo '</tr>';
+//     foreach ($routeCollection as $value) {
+//         echo '<tr>';
+//         echo '<td>'.$value->getMethods()[0].'</td>';
+//         echo '<td>'.$value->getName().'</td>';
+//         echo '<td>'.$value->getPath().'</td>';
+//         echo '<td>'.$value->getActionName().'</td>';
+//         echo '</tr>';
+//     }
+//     echo '</table>';
+// });
 
 /*
   |=============================================================
