6def37b224e97e773d84301bc6ff0ada300ac2b6
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-06-15 15:45:46 +0530

    update v1.0.7.1

--- DB/update from 1.0.7.4 to 1.0.7.5/Updatedatabase.sql
+++ DB/update from 1.0.7.4 to 1.0.7.5/Updatedatabase.sql
@@ -587,11 +587,16 @@ ALTER TABLE `settings_system`
 -- Dumping data for table `system_settings`
 --
 
-UPDATE `settings_system` SET `version`='1.0.7.5' WHERE 1
+UPDATE `settings_system` SET `version`='1.0.7.5' WHERE 1;
 
 
 ALTER TABLE `settings_ticket`
   ADD `lock_ticket_frequency` varchar(222) NOT NULL AFTER `max_file_size`;
 
 
-UPDATE `settings_ticket` SET `lock_ticket_frequency`='0' WHERE 1
+UPDATE `settings_ticket` SET `lock_ticket_frequency`='0' WHERE 1;
+
+
+TRUNCATE TABLE `user_notification`;
+
+TRUNCATE TABLE `notifications`;
