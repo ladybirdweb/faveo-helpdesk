27d6eb9022dadb17ab4fbbd6045896b887a4de6a
Manish Verma <manish.verma@ladybirdweb.com>
2016-06-20 17:54:42 +0530

    Installer patch

--- app/Http/Controllers/Admin/helpdesk/ErrorAndDebuggingController.php
+++ app/Http/Controllers/Admin/helpdesk/ErrorAndDebuggingController.php
@@ -60,26 +60,18 @@ class ErrorAndDebuggingController extends Controller
             $bugsnag_debug = ($bugsnag_debug) ? 'true' : 'false';
             if ($debug != \Input::get('debug') || $bugsnag_debug != \Input::get('bugsnag')) {
                 // dd($request->input());
-                $debug_new = base_path()
-                             .DIRECTORY_SEPARATOR.
-                             'config'
-                             .DIRECTORY_SEPARATOR.
-                             'app.php';
+                $debug_new = base_path().DIRECTORY_SEPARATOR.'.env';
                 $datacontent = File::get($debug_new);
-                $datacontent = str_replace("'debug' => ".$debug,
-                                           "'debug' => ".\Input::get('debug'),
+                $datacontent = str_replace("APP_DEBUG=".$debug,
+                                           "APP_DEBUG=".\Input::get('debug'),
                                             $datacontent);
                 File::put($debug_new, $datacontent);
 
                 // dd($request->input());
-                $bugsnag_debug_new = base_path()
-                             .DIRECTORY_SEPARATOR.
-                             'config'
-                             .DIRECTORY_SEPARATOR.
-                             'app.php';
+                $bugsnag_debug_new = base_path().DIRECTORY_SEPARATOR.'.env';
                 $datacontent2 = File::get($bugsnag_debug_new);
-                $datacontent2 = str_replace("'bugsnag_reporting' => ".$bugsnag_debug,
-                                           "'bugsnag_reporting' => ".\Input::get('bugsnag'),
+                $datacontent2 = str_replace("APP_BUGSNAG=".$bugsnag_debug,
+                                           "APP_BUGSNAG=".\Input::get('bugsnag'),
                                             $datacontent2);
                 File::put($bugsnag_debug_new, $datacontent2);
 
