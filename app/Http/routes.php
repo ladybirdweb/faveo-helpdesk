268b7b13f610d7a5b60739d8fc1c6ef6f8abad6b
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-06-21 10:14:51 -0400

    Applied fixes from StyleCI

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 Route::group(['middleware' => ['web']], function () {
     Route::group(['middleware' => 'update'], function () {
         Route::controllers([
-            'auth' => 'Auth\AuthController',
+            'auth'     => 'Auth\AuthController',
             'password' => 'Auth\PasswordController',
         ]);
     });
@@ -545,11 +545,11 @@ Route::group(['middleware' => ['web']], function () {
         });
         //route for submit error and debugging setting form page
         Route::post('post-settings', ['as' => 'post.error.debug.settings',
-            'uses' => 'Admin\helpdesk\ErrorAndDebuggingController@postSettings',]);
+            'uses'                         => 'Admin\helpdesk\ErrorAndDebuggingController@postSettings', ]);
 
         //route to error logs table page
         Route::get('show-error-logs', [
-            'as' => 'error.logs',
+            'as'   => 'error.logs',
             'uses' => 'Admin\helpdesk\ErrorAndDebuggingController@showErrorLogs',
         ]);
     });
@@ -889,7 +889,7 @@ Route::group(['middleware' => ['web']], function () {
     });
     Route::any('getdata', function () {
         $term = Illuminate\Support\Str::lower(Input::get('term'));
-        $data = Illuminate\Support\Facades\DB::table('tickets')->distinct()->select('ticket_number')->where('ticket_number', 'LIKE', $term . '%')->groupBy('ticket_number')->take(10)->get();
+        $data = Illuminate\Support\Facades\DB::table('tickets')->distinct()->select('ticket_number')->where('ticket_number', 'LIKE', $term.'%')->groupBy('ticket_number')->take(10)->get();
         foreach ($data as $v) {
             return [
                 'value' => $v->ticket_number,
@@ -1039,24 +1039,24 @@ Route::group(['middleware' => ['web']], function () {
       |=============================================================
      */
     Route::get('500', ['as' => 'error500', function () {
-    return view('errors.500');
-}]);
+        return view('errors.500');
+    }]);
 
     Breadcrumbs::register('error500', function ($breadcrumbs) {
         $breadcrumbs->push('500');
     });
 
     Route::get('404', ['as' => 'error404', function () {
-    return view('errors.404');
-}]);
+        return view('errors.404');
+    }]);
 
     Breadcrumbs::register('error404', function ($breadcrumbs) {
         $breadcrumbs->push('404');
     });
 
     Route::get('board-offline', ['as' => 'board.offline', function () {
-    return view('errors.offline');
-}]);
+        return view('errors.offline');
+    }]);
 
     Breadcrumbs::register('board.offline', function ($breadcrumbs) {
         $breadcrumbs->push('Board Offline');
