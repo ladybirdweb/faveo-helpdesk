b78fdeee7283268f5fafc724b58cd4b2af5636d2
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-07-12 18:46:36 +0530

    update v1.0.7.9 update for podio

--- resources/views/themes/default1/agent/helpdesk/ticket/timeline.blade.php
+++ resources/views/themes/default1/agent/helpdesk/ticket/timeline.blade.php
@@ -236,7 +236,7 @@ if ($thread->title != "") {
                         <?php
                         $user_phone = App\User::where('mobile', '=', $thread->user_id)->first();
                         $TicketData = App\Model\helpdesk\Ticket\Ticket_Thread::where('ticket_id', '=', $thread->ticket_id)->max('id');
-                        $TicketDatarow = App\Model\helpdesk\Ticket\Ticket_Thread::where('id', '=', $TicketData)->first();
+                        $TicketDatarow = App\Model\helpdesk\Ticket\Ticket_Thread::where('id', '=', $TicketData)->where('is_internal', '=', 0)->first();
                         $LastResponse = App\User::where('id', '=', $TicketDatarow->user_id)->first();
                         if ($LastResponse->role == "user") {
                             $rep = "#F39C12";
