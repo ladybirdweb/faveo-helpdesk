7b326677f22f0ff5c76022079864df14a60e4127
Old-Fart <mathieu@zeroserieux.com>
2016-07-12 02:13:18 -0400

    Fixes broken links
    Fixes broken links to plugin by adding lb-faveo/ directory to templates

--- resources/views/themes/default1/layouts/kb.blade.php
+++ resources/views/themes/default1/layouts/kb.blade.php
@@ -11,23 +11,23 @@
         <!-- Ionicons -->
         <link href="{{asset("downloads/ionicons.min.css")}}" rel="stylesheet" type="text/css" />
         <!-- fullCalendar 2.2.5-->
-        <link href="{{asset("plugins/fullcalendar/fullcalendar.min.css")}}" rel="stylesheet" type="text/css" />
-        <link href="{{asset("plugins/fullcalendar/fullcalendar.print.css")}}" rel="stylesheet" type="text/css" media='print' />
+        <link href="{{asset("lb-faveo/plugins/fullcalendar/fullcalendar.min.css")}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset("lb-faveo/plugins/fullcalendar/fullcalendar.print.css")}}" rel="stylesheet" type="text/css" media='print' />
         <!-- Theme style -->
         <link href="{{asset("dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
         <!-- AdminLTE Skins. Choose a skin from the css/skins folder instead of downloading all of them to reduce the load. -->
         <link href="{{asset("dist/css/skins/_all-skins.min.css")}}" rel="stylesheet" type="text/css" />
         <!-- iCheck -->
-        <link href="{{asset("plugins/iCheck/flat/blue.css")}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset("lb-faveo/plugins/iCheck/flat/blue.css")}}" rel="stylesheet" type="text/css" />
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <link rel="stylesheet" href="{{asset("dist/css/tabby.css")}}" type="text/css">
         <link href="{{asset("downloads/jquerysctipttop.css")}}" rel="stylesheet" type="text/css">
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
         <link rel="stylesheet" href="{{asset("dist/css/editor.css")}}" type="text/css">
-        <link href="{{asset("plugins/filebrowser/plugin.js")}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset("lb-faveo/plugins/filebrowser/plugin.js")}}" rel="stylesheet" type="text/css" />
         {{-- jquery ui css --}}
         <link type="text/css" href="{{asset("downloads/jquery.ui.css")}}" rel="stylesheet">
-        <link href="{{asset("plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css")}}" rel="stylesheet" type="text/css" />        
+        <link href="{{asset("lb-faveo/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css")}}" rel="stylesheet" type="text/css" />
         <!-- <link type="text/css" href="http://code.jquery.com/ui/1.9.1/themes/redmond/jquery-ui.css" rel="stylesheet"> -->
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -65,14 +65,14 @@ if ($company != null) {
                                         <img src="{{asset('dist/img')}}{{'/'}}{{Auth::user()->profile_pic}}"class="user-image" alt="User Image"/>
                                     @endif
                                     <span class="hidden-xs">{{Auth::user()->first_name}}</span>
-                                @endif          
+                                @endif
                                 </a>
                                 <ul class="dropdown-menu">
                                     <!-- User image -->
                                     <li class="user-header"  style="background-color:#343F44;">
                                         @if(Auth::user()->profile_pic)
                                         <img src="{{asset('dist/img')}}{{'/'}}{{Auth::user()->profile_pic}}" class="img-circle" alt="User Image" />
-                                        @else                                      
+                                        @else
                                             <img src="{{ Gravatar::src(Auth::user()->email) }}" class="img-circle" alt="User Image">
                                         @endif
                                         <p>
@@ -165,7 +165,7 @@ if ($company != null) {
                                                 <small class="label pull-right bg-green"></small>
                                             </a>
                                         </li>
-                                        
+
                             </ul>
                         </section>
                         <!-- /.sidebar -->
@@ -194,15 +194,15 @@ if ($company != null) {
                     <!-- Bootstrap 3.3.2 JS -->
                     <script src="{{asset("downloads/bootstrap.min.js")}}" type="text/javascript"></script>
                     <!-- Slimscroll -->
-                    <script src="{{asset("plugins/slimScroll/jquery.slimscroll.min.js")}}" type="text/javascript"></script>
+                    <script src="{{asset("lb-faveo/plugins/slimScroll/jquery.slimscroll.min.js")}}" type="text/javascript"></script>
                     <!-- FastClick -->
-                    <script src="{{asset("plugins/fastclick/fastclick.min.js")}}"></script>
+                    <script src="{{asset("lb-faveo/plugins/fastclick/fastclick.min.js")}}"></script>
                     <!-- AdminLTE App -->
                     <script src="{{asset("dist/js/app.min.js")}}" type="text/javascript"></script>
                     <!-- AdminLTE for demo purposes -->
                     <script src="{{asset("dist/js/demo.js")}}" type="text/javascript"></script>
                     <!-- iCheck -->
-                    <script src="{{asset("plugins/iCheck/icheck.min.js")}}" type="text/javascript"></script>
+                    <script src="{{asset("lb-faveo/plugins/iCheck/icheck.min.js")}}" type="text/javascript"></script>
                     <!-- maskinput -->
                     <script src="js/jquery.maskedinput.min.js" type="text/javascript"></script>
                      <!-- jquery ui  -->
@@ -258,8 +258,8 @@ $(function() {
                     <!-- CK Editor -->
                     <!-- // <script src="{{asset("//cdn.ckeditor.com/4.4.3/standard/ckeditor.js")}}"></script> -->
                     <script src="{{asset("downloads/CKEditor.js")}}"></script>
-                    <script src="{{asset("plugins/filebrowser/plugin.js")}}"></script>
-                    <script src="{{asset("plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js")}}" type="text/javascript"></script>        
+                    <script src="{{asset("lb-faveo/plugins/filebrowser/plugin.js")}}"></script>
+                    <script src="{{asset("lb-faveo/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js")}}" type="text/javascript"></script>
                     <script>
                         $(function () {
                         //Add text editor
