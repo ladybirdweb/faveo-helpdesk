0a0fdd46885a0aa775998f72cabd4ffa01a37fdc
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-07-25 12:23:30 +0530

    updated installaion fix

--- database/migrations/2016_06_28_141613_version1079table.php
+++ database/migrations/2016_06_28_141613_version1079table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Version1079 extends Migration
+class Version1079table extends Migration
 {
     /**
      * Run the migrations.
