d8d3260b7047a348d5ee5e17620aec2291e302f7
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-08-04 20:05:05 +0530

    Update v1.0.7.9

--- DB/update from 1.0.7.8 to 1.0.7.9/Updatedatabase.sql
+++ DB/update from 1.0.7.8 to 1.0.7.9/Updatedatabase.sql
@@ -3,6 +3,9 @@
 --
 -- Alter Table structure for table `common_settings`
 --
-INSERT INTO `common_settings` (`id`, `option_name`, `option_value`, `status`, `optional_field`, `created_at`, `updated_at`) VALUES (2, 'enable_rtl', '', '', '', '2016-06-14 09:07:17', '2016-06-14 09:07:17'),(3, 'user_set_ticket_status', '',1,'','2016-06-14 09:07:17','2016-06-14 09:07:17');
-
--- Wait till the official release
+INSERT INTO `common_settings` (`id`, `option_name`, `option_value`, `status`, `optional_field`, `created_at`, `updated_at`)
+VALUES (2, 'enable_rtl', '', '', '', '2016-06-14 09:07:17', '2016-06-14 09:07:17'),
+       (3, 'user_set_ticket_status', '',1,'','2016-06-14 09:07:17','2016-06-14 09:07:17'),
+       (4, 'send_otp', '',0,'','2016-06-14 09:07:17','2016-06-14 09:07:17');
+INSERT INTO `ticket_status` (`id`, `name`, `state`, `mode`, `message`, `flags`, `sort`, `email_user`, `icon_class`, `properties`, `created_at`, `updated_at`)
+VALUES (6, 'Unverified', 'unverified', 3, 'User account verification required.', 0, 6, 0, '', 'Ticket will be open after user verifies his/her account.', '2016-06-14 09:07:04', '2016-06-14 09:07:04');
