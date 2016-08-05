09bf25b5e2826812fd8ae10f429d5be873048072
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-08-05 16:34:31 +0530

    update v1.0.7.9

--- resources/views/themes/default1/client/helpdesk/form.blade.php
+++ resources/views/themes/default1/client/helpdesk/form.blade.php
@@ -170,7 +170,7 @@ class = "active"
             <?php Event::fire(new App\Events\ClientTicketForm()); ?>
             <div class="col-md-12" id="response"> </div>
             <div id="ss" class="xs-md-6 form-group {{ $errors->has('') ? 'has-error' : '' }}"> </div>
-            <div class="col-md-12 form-group">{!! Form::submit(Lang::get('lang.Send'),['class'=>'form-group btn btn-info pull-left'])!!}</div>
+            <div class="col-md-12 form-group">{!! Form::submit(Lang::get('lang.Send'),['class'=>'form-group btn btn-info pull-left', 'onclick' => 'this.disabled=true;this.value="Sending, please wait...";this.form.submit();'])!!}</div></div>
         </div>
         <div class="col-md-12" id="response"> </div>
         <div id="ss" class="xs-md-6 form-group {{ $errors->has('') ? 'has-error' : '' }}"> </div>
