a51c7fa4e09643e7a8e5a6e8dd7639f82ca59e94
Sujit Prasad <sujit.prasad@ladybirdweb.com>
2016-08-08 15:28:11 +0530

    Git push fix
    CreateTicketRequest.php

--- app/Http/Requests/helpdesk/CreateTicketRequest.php
+++ app/Http/Requests/helpdesk/CreateTicketRequest.php
@@ -1,9 +1,7 @@
 <?php
-
 namespace App\Http\Requests\helpdesk;
-
 use App\Http\Requests\Request;
-
+use App\Model\helpdesk\Settings\CommonSettings;
 /**
  * CreateTicketRequest.
  *
@@ -20,7 +18,6 @@ class CreateTicketRequest extends Request
     {
         return true;
     }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -28,8 +25,11 @@ class CreateTicketRequest extends Request
      */
     public function rules()
     {
+        $check = $this->check(new CommonSettings);
+        if ($check != 0) {
+            return $check;
+        }
         return [
-
             'email'     => 'required|email|max:60',
             'first_name'  => 'required|min:3|max:40',
             'helptopic' => 'required',
@@ -39,5 +39,31 @@ class CreateTicketRequest extends Request
             'body'     => 'required|min:10',
             'priority' => 'required',
         ];
+    }
+    /**
+     *@category Funcion to set rule if send opt is enabled
+     *@param Object $settings (instance of Model common settings)
+     *@author manish.verma@ladybirdweb.com
+     *@return array|int 
+     */
+    public function check($settings)
+    {
+        $settings = $settings->select('status')->where('option_name', '=', 'send_otp')->first();
+        if ($settings->status == '1' || $settings->status == 1) {
+            return [
+                'email'     => 'required|email|max:60',
+                'first_name'  => 'required|min:3|max:40',
+                'helptopic' => 'required',
+                // 'dept' => 'required',
+                'sla'      => 'required',
+                'subject'  => 'required|min:5',
+                'body'     => 'required|min:10',
+                'priority' => 'required',
+                'code'     => 'required',
+                'mobile'   => 'required',
+            ];
+        } else {
+            return 0;
+        }
     }
 }
