dbb118b0045094f5e2567184786d23230c4353f3
Manish Verma <manish.verma@ladybirdweb.com>
2016-11-02 17:35:38 +0530

    Bug-fix-patch
    chaged 'user/settings/priority'  to 'user_settings_priority' in ajax
    call

--- resources/views/themes/default1/admin/helpdesk/manage/ticket_priority/index.blade.php
+++ resources/views/themes/default1/admin/helpdesk/manage/ticket_priority/index.blade.php
@@ -124,7 +124,7 @@ class="active"
         $.ajax({
             type: 'post',
             url: '{{route("user.priority.index")}}',
-            data: {user/settings/priority: user_settings_priority},
+            data: {user_settings_priority: user_settings_priority},
             success: function (result) {
                 // with('success', Lang::get('lang.approval_settings-created-successfully'));
                 // alert("Hi, testing");
