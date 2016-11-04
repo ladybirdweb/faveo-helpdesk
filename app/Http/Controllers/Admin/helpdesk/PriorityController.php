e4b975529462b9bbf0a8ff72fdb4fa3373085b7c
arindam85 <arindam.bg91@gmail.com>
2016-11-04 16:27:24 +0530

    arindam-priority-complie

--- app/Http/Controllers/Admin/helpdesk/PriorityController.php
+++ app/Http/Controllers/Admin/helpdesk/PriorityController.php
@@ -159,7 +159,7 @@ class PriorityController extends Controller
             Ticket_Priority::where('is_default', '>', '0')
                     ->update(['is_default' => '0']);
             Ticket_Priority::where('priority_id', '=', $priority_id)
-                    ->update(['is_default' => $priority_id]);
+                    ->update(['is_default' => 1]);
         }
 
         return \Redirect::route('priority.index')->with('success', (Lang::get('lang.priority_successfully_updated')));
