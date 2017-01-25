1995c6f22feb3c94a2dd6087db3f484236414189
Manish Verma <manish.verma@ladybirdweb.com>
2017-01-25 13:36:17 +0530

    v1.9.3 updates

--- config/app.php
+++ config/app.php
@@ -33,7 +33,7 @@ return [
       | This tells about aplication current version.
       |
      */
-    'version' => 'Community 1.9.2',
+    'version' => 'Community 1.9.3',
     /*
       |--------------------------------------------------------------------------
       | Application Name
