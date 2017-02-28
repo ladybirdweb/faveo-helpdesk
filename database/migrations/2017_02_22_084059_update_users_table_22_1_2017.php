cfc0f0d21159f63158e7cd86c7d0e70e8907eeec
Manish Verma <mverma16@outlook.com>
2017-02-28 07:03:45 +0000

    Apply fixes from StyleCI

--- database/migrations/2017_02_22_084059_update_users_table_22_1_2017.php
+++ database/migrations/2017_02_22_084059_update_users_table_22_1_2017.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class UpdateUsersTable2212017 extends Migration
 {
