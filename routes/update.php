3ee47eb9a19c2a77039cb7466e7e3a61c350645b
vijaysebastian <vijaysebastian111@gmail.com>
2017-10-12 05:38:53 +0000

    Apply fixes from StyleCI

--- routes/update.php
+++ routes/update.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * upgrade from 1.9.6 to 1.9.7
+ * upgrade from 1.9.6 to 1.9.7.
  */
-Route::get('1-9-7',function(){
-    \Artisan::call('migrate',['--force'=>true]);
-    \Artisan::call('db:seed',['--class'=>'TickettypeSeeder','--force'=>true]);
-    \Artisan::call('db:seed',['--class'=>'CustomFormSeeder','--force'=>true]);
+Route::get('1-9-7', function () {
+    \Artisan::call('migrate', ['--force'=>true]);
+    \Artisan::call('db:seed', ['--class'=>'TickettypeSeeder', '--force'=>true]);
+    \Artisan::call('db:seed', ['--class'=>'CustomFormSeeder', '--force'=>true]);
+
     return redirect('/')->with(['success'=>'Application has upgraded to 1.9.7 from 1.9.6']);
 });
-
