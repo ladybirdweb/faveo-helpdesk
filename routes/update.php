0372de5d1a670503e8b382619b54c7965d94fe1b
Vijay Sebastian <vijay.sebastian@ladybirdweb.com>
2017-10-12 12:11:53 +0530

    updates

--- routes/update.php
+++ routes/update.php
@@ -3,6 +3,9 @@
  * upgrade from 1.9.6 to 1.9.7.
  */
 Route::get('1-9-7',function(){
+    if(\Schema::hasTable('ticket_type')){
+        return redirect('/')->with(['success'=>'You are application is up to date']);
+    }
     \Artisan::call('migrate',['--force'=>true]);
     \Artisan::call('db:seed',['--class'=>'TickettypeSeeder','--force'=>true]);
     \Artisan::call('db:seed',['--class'=>'CustomFormSeeder','--force'=>true]);
