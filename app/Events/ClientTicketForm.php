0e469e6c3a2a828cc778336078a6dd9c3d1371a5
Manish Verma <alerts@ladybirdweb.com>
2018-08-02 14:24:02 +0530

    client panel ticket form fix
    Fixed issue in loading create ticket form in client panel

--- app/Events/ClientTicketForm.php
+++ app/Events/ClientTicketForm.php
@@ -8,10 +8,18 @@ class ClientTicketForm extends Event
 {
     use SerializesModels;
 
-    public $event;
+    public $para1;
+    public $para2;
+    public $para3;
+    public $para4;
+    public $para5;
 
-    public function __construct($event)
+    public function __construct($para1 = '', $para2 = '', $para3 = '', $para4 = '', $para5 = '')
     {
-        $this->event = $event;
+        $this->para1 = $para1;
+        $this->para2 = $para2;
+        $this->para3 = $para3;
+        $this->para4 = $para4;
+        $this->para5 = $para5;
     }
 }
