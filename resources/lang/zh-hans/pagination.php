92941a374ec556144b51e0f0661f4509bb490e1e
Manish Verma <alerts@ladybirdweb.com>
2018-08-09 17:57:21 +0530

    Chinese language
    Added chinese language provided by Yang.l

---
+++ resources/lang/zh-hans/pagination.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  |--------------------------------------
+  |   中文语言包
+  |--------------------------------------
+ */
+
+return [
+
+    /*
+      |--------------------------------------------------------------------------
+      | Pagination Language Lines
+      |--------------------------------------------------------------------------
+      |
+      | The following language lines are used by the paginator library to build
+      | the simple pagination links. You are free to change them to anything
+      | you want to customize your views to better match your application.
+      |
+     */
+
+    'previous' => '&laquo; Previous',
+    'next'     => 'Next &raquo;',
+];
---
+++ resources/lang/zh-hans/password.php
@@ -0,0 +1,26 @@
+<?php
+/*
+  |--------------------------------------
+  |   中文语言包
+  |--------------------------------------
+ */
+
+return [
+
+    /*
+      |--------------------------------------------------------------------------
+      | Password Reminder Language Lines
+      |--------------------------------------------------------------------------
+      |
+      | The following language lines are the default lines which match reasons
+      | that are given by the password broker for a password update attempt
+      | has failed, such as for an invalid token or invalid new password.
+      |
+     */
+
+    'password' => '密码必须六位并且正确.',
+    'user'     => '邮件地址不存在.',
+    'token'    => '密码重置令牌无效.',
+    'sent'     => '我们已经发送了一个密码重置连接!',
+    'reset'    => '您的的密码已经重置!',
+];
