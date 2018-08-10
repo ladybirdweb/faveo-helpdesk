b578bf5f35c9ff1f2b3bc0891a149d3a235c4fd6
Manish Verma <manish.verma@ladybirdweb.com>
2018-08-10 10:02:15 +0000

    Apply fixes from StyleCI

--- database/migrations/2016_07_26_095020_create_queue_services_table.php
+++ database/migrations/2016_07_26_095020_create_queue_services_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Model\MailJob\QueueService;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
--- database/seeds/TicketSourceSeeder.php
+++ database/seeds/TicketSourceSeeder.php
@@ -1,11 +1,10 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\Model\helpdesk\Ticket\Ticket_source;
+use Illuminate\Database\Seeder;
 
 class TicketSourceSeeder extends Seeder
 {
-
     /**
      * Run the database seeds.
      *
@@ -16,13 +15,12 @@ class TicketSourceSeeder extends Seeder
         \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Ticket_source::truncate();
         \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-        Ticket_source::create(['name' => 'Web', 'value' => 'Web', 'css_class' => 'fa fa-globe',]);
-        Ticket_source::create(['name' => 'Email', 'value' => 'E-mail', 'css_class' => 'fa fa-envelope',]);
-        Ticket_source::create(['name' => 'Agent', 'value' => 'Agent Panel', 'css_class' => 'fa fa-user',]);
-        Ticket_source::create(['name' => 'Facebook', 'value' => 'Facebook', 'css_class' => 'fa fa-facebook',]);
-        Ticket_source::create(['name' => 'Twitter', 'value' => 'Twitter', 'css_class' => 'fa fa-twitter',]);
-        Ticket_source::create(['name' => 'Call', 'value' => 'Call', 'css_class' => 'fa fa-phone',]);
-        Ticket_source::create(['name' => 'Chat', 'value' => 'Chat', 'css_class' => 'fa fa-comment',]);
+        Ticket_source::create(['name' => 'Web', 'value' => 'Web', 'css_class' => 'fa fa-globe']);
+        Ticket_source::create(['name' => 'Email', 'value' => 'E-mail', 'css_class' => 'fa fa-envelope']);
+        Ticket_source::create(['name' => 'Agent', 'value' => 'Agent Panel', 'css_class' => 'fa fa-user']);
+        Ticket_source::create(['name' => 'Facebook', 'value' => 'Facebook', 'css_class' => 'fa fa-facebook']);
+        Ticket_source::create(['name' => 'Twitter', 'value' => 'Twitter', 'css_class' => 'fa fa-twitter']);
+        Ticket_source::create(['name' => 'Call', 'value' => 'Call', 'css_class' => 'fa fa-phone']);
+        Ticket_source::create(['name' => 'Chat', 'value' => 'Chat', 'css_class' => 'fa fa-comment']);
     }
-
 }
