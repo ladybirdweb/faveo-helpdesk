cc77ee31cb520ad9769f021bc8c37ed685da1c3c
Manish Verma <alerts@ladybirdweb.com>
2018-08-23 13:41:40 +0530

    Removed sla-escalate command from kernal
    Removed sla-escalate atrisan command from kernal as the functionality is not working and which is causing the issue while running cron command in v1.10

--- routes/console.php
+++ routes/console.php
@@ -21,10 +21,6 @@ Artisan::command('mac-update', function () {
     $emails = new App\Model\helpdesk\Email\Emails();
     $emails->update(['password'=>encrypt('')]);
 })->describe('Updating encrypted value to null');
-Artisan::command('sla-escalate', function () {
-    $noti = new \App\Http\Controllers\Agent\helpdesk\Notifications\NotificationController();
-    $noti->notificationSla();
-})->describe('to send notification for sla due');
 
 /*
  * Command for pre install check
