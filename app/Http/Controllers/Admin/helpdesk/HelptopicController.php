ee1f820e51a16158f7451bde21653e9a45ff5b95
arindam85 <arindam.bg91@gmail.com>
2018-08-27 11:21:24 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Admin/helpdesk/HelptopicController.php
+++ app/Http/Controllers/Admin/helpdesk/HelptopicController.php
@@ -192,8 +192,7 @@ class HelptopicController extends Controller
                 \DB::table('settings_ticket')
                     ->where('id', '=', 1)
                     ->update(['help_topic' => $id]);
-            Help_topic::where('id', $id)->update(['status'=>1,'type'=>1]);
-
+                Help_topic::where('id', $id)->update(['status'=>1, 'type'=>1]);
             }
             /* redirect to Index page with Success Message */
             return redirect('helptopic')->with('success', Lang::get('lang.helptopic_updated_successfully'));
@@ -213,7 +212,7 @@ class HelptopicController extends Controller
      */
     public function destroy($id, Help_topic $topic, Ticket $ticket_setting)
     {
-       $ticket_settings = $ticket_setting->where('id', '=', '1')->first();
+        $ticket_settings = $ticket_setting->where('id', '=', '1')->first();
         if ($ticket_settings->help_topic == $id) {
             return redirect('departments')->with('fails', Lang::get('lang.you_cannot_delete_default_department'));
         } else {
