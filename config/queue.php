54eba62852e75ac7dc2add447b7d26501f364fe2
Jelle Dijkhuizen <jelle@dijkma.nl>
2019-02-10 22:36:14 +0100

    still wierd problems

--- config/queue.php
+++ config/queue.php
@@ -63,7 +63,7 @@ return [
         'redis' => [
             'driver' => 'redis',
             'connection' => 'default',
-            'queue' => 'support',
+            'queue' => 'default',
             'retry_after' => 90,
         ],
     ],
--- config/services.php
+++ config/services.php
@@ -15,8 +15,8 @@ return [
      */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+        'domain' => '',
+        'secret' => '',
     ],
 
     'ses' => [
