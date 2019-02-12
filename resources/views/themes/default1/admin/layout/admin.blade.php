277db409ca21ae7bedab35cfac2fe47261a189a6
Jelle Dijkhuizen <jelle@dijkma.nl>
2019-02-12 15:23:39 +0100

    Update admin.blade.php

--- resources/views/themes/default1/admin/layout/admin.blade.php
+++ resources/views/themes/default1/admin/layout/admin.blade.php
@@ -475,11 +475,9 @@
         });</script>
 
     <script src="{{asset("lb-faveo/js/tabby.js")}}"></script>
-    <!-- CK Editor -->
-        <script src="{{asset("//cdn.ckeditor.com/4.4.3/standard/ckeditor.js")}}"></script>
-        <script src="{{asset("//cdn.ckeditor.com/4.4.3/full/ckeditor.js")}}"></script>
-        <script src="{{asset("lb-faveo/plugins/filebrowser/plugin.js")}}"></script>
-        <!-- <script src="{{asset("lb-faveo/plugins/filebrowser/plugin.js")}}"></script>
+    <!-- CK Editor
+    <script src="{{asset("//cdn.ckeditor.com/4.4.3/standard/ckeditor.js")}}"></script>
+    <script src="{{asset("lb-faveo/plugins/filebrowser/plugin.js")}}"></script>
     <script src="{{asset("lb-faveo/js/languagechanger.js")}}" type="text/javascript"></script> -->
     @yield('FooterInclude')
 </body>
