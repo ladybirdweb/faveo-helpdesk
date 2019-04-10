4f7e0296a6bae4a889e54dcf2eeeb3687bdc2c8e
Manish Verma <alerts@ladybirdweb.com>
2019-04-10 15:26:17 +0530

    v1.10.5 updates
    update
    
    Fixes testclasses
    
    Apply fixes from StyleCI
    
    Update .travis.yml
    
    Revert "Update .travis.yml"
    
    This reverts commit 462dd1cb44402f6cbcb516ff17e48d1d562e98e2.

--- config/app.php
+++ config/app.php
@@ -33,7 +33,7 @@ return [
       | This tells about aplication current version.
       |
      */
-    'version' => 'Community 1.10.4',
+    'version' => 'Community 1.10.5',
     /*
       |--------------------------------------------------------------------------
       | Application Name
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Tests;
+
 class ExampleTest extends TestCase
 {
     /**
