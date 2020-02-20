e89b4f0646ba84bc04c212bbb1be3215f0e4a909
Bhanu <bhanu@ladybirdweb.com>
2020-02-20 05:27:04 +0000

    Apply fixes from StyleCI

--- app/Model/helpdesk/NotificationOld/UserNotification.php
+++ app/Model/helpdesk/NotificationOld/UserNotification.php
@@ -9,6 +9,6 @@ class UserNotification extends BaseModel
     protected $table = 'user_notification';
     protected $fillable = [
 
-            'notification_id', 'user_id', 'is_read',
-                            ];
+        'notification_id', 'user_id', 'is_read',
+    ];
 }
--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -16,15 +16,15 @@ class UserSeeder extends Seeder
         $str = 'demopass';
         $password = \Hash::make($str);
         $user = User::create([
-                    'first_name'   => 'Demo',
-                    'last_name'    => 'Admin',
-                    'email'        => null,
-                    'user_name'    => 'demo_admin',
-                    'password'     => $password,
-                    'assign_group' => 1,
-                    'primary_dpt'  => 1,
-                    'active'       => 1,
-                    'role'         => 'admin',
+            'first_name'   => 'Demo',
+            'last_name'    => 'Admin',
+            'email'        => null,
+            'user_name'    => 'demo_admin',
+            'password'     => $password,
+            'assign_group' => 1,
+            'primary_dpt'  => 1,
+            'active'       => 1,
+            'role'         => 'admin',
         ]);
         // checking if the user have been created
     }
