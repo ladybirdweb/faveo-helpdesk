720a76c4da3f8c6ad97a93804f39eee6cd1e462b
Shift <shift@laravelshift.com>
2023-01-03 08:25:49 +0000

    Shift middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,4 +68,19 @@ class Kernel extends HttpKernel
         'installer' => \App\Http\Middleware\IsInstalled::class,
         'force.option' => \App\Http\Middleware\TicketViewURL::class,
     ];
+    /**
+     * The priority-sorted list of middleware.
+     *
+     * This forces non-global middleware to always be in the given order.
+     *
+     * @var array
+     */
+    protected $middlewarePriority = [
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \App\Http\Middleware\Authenticate::class,
+        \Illuminate\Session\Middleware\AuthenticateSession::class,
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        \Illuminate\Auth\Middleware\Authorize::class,
+    ];
 }
