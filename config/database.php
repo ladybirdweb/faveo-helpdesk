7088bb304be1ac3bb8e8296bb31bff6994be3834
RafficMohammed <mohammed.raffic@ladybirdweb.com>
2023-02-13 13:15:07 +0530

    adding-engine

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'prefix'         => '',
             'prefix_indexes' => true,
             'strict'         => false,
-            'engine'         => null,
+            'engine'         => env('DB_ENGINE', null),
             'options'        => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
