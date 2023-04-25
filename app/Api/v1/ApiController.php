237dbcf61555fa06050138cd2de169299ac6a4a9
noor <noor.a@ladybirdweb.com>
2023-04-25 14:27:37 +0530

    changes

--- app/Api/v1/ApiController.php
+++ app/Api/v1/ApiController.php
@@ -169,12 +169,10 @@ class ApiController extends Controller
             $core = new CoreTicketController($PhpMailController, $NotificationController);
             $this->request->merge(['body' => preg_replace('/[ ](?=[^>]*(?:<|$))/', '&nbsp;', nl2br($this->request->get('body')))]);
             $request->replace($this->request->except('token', 'api_key'));
-            $request->merge(['token' => '']);
-
             $request_data = $request->except(['token']);
-            $response = $core->post_newticket(new CreateTicketRequest($request_data), $code, true);
+            $response = $core->post_newticket(new CreateTicketRequest($request->except('token','api_key')), $code, true);
 
-            return response()->json(compact('response'));            //$response = $this->ticket->createTicket($user_id, $subject, $body, $helptopic, $sla, $priority, $source, $headers, $dept, $assignto, $form_data, $attach);
+            //$response = $this->ticket->createTicket($user_id, $subject, $body, $helptopic, $sla, $priority, $source, $headers, $dept, $assignto, $form_data, $attach);
             //return $response;
             /*
              * return ticket details
