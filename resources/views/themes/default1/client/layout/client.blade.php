07caf197ccaed02fa4f38b9dc9884d7c6930ea3a
Muhammed Can <mucan54@hotmail.com>
2023-06-18 11:41:02 +0300

    Mobile menu button fix.

--- resources/views/themes/default1/client/layout/client.blade.php
+++ resources/views/themes/default1/client/layout/client.blade.php
@@ -132,7 +132,7 @@
                             </a>
                         </div>
 
-                        <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                        <button class="navbar-toggler custom-toggler" onclick="javascript:(function() { jQuery('#navbarSupportedContent').toggle(); })()" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
                             <span class="navbar-toggler-icon"></span>
