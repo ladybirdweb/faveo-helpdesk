9cfc03133fe21d582776cad16a5c233bb814bcbd
KNaveenraj-ladybird <K.naveenraj@ladybirdweb.com>
2023-10-25 18:13:25 +0530

    commit-3

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,10 +2,6 @@
 
 namespace Tests\Unit;
 
-use App\User;
-use Faker\Factory as FakerFactory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -18,33 +14,5 @@ class ExampleTest extends TestCase
     public function test_example()
     {
         $this->assertTrue(true);
-    }
-
-    public function test_store_user()
-    {
-        $faker = FakerFactory::create();
-
-        //Create User -> Agent
-        $str = Str::random(10);
-        $password = Hash::make($str);
-        $email = $faker->unique()->email();
-        $user = new User([
-            'first_name'   => $faker->firstName(),
-            'last_name'    => $faker->lastName(),
-            'email'        => $email,
-            'user_name'    => $faker->unique()->userName(),
-            'password'     => $password,
-            'active'       => 1,
-            'role'         => 'user',
-        ]);
-        $user->save();
-
-        // Check if data is inserted
-        $this->assertDatabaseHas('users', ['email' => $email]);
-
-        // Authenticate as the created user
-        $this->actingAs($user);
-
-        $this->assertAuthenticated();
     }
 }
